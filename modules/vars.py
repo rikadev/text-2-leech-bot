c597a420b3c8a18cac330a7e8b645ad9205823a8
rikadev <167677871+rikadev@users.noreply.github.com>
2024-06-17 08:25:18 -0700

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,6 +1,6 @@
 # SudoR2spr WOODcraft
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "19178442"
-api_hash = "772c72ec782d0873c7a1e4053c42934d"
-bot_token = "7215138170:AAH_x06XGjA_p2yc27uW_kJ4C41Odtv-55s"
+api_id = "********"
+api_hash = "**************************"
+bot_token = "************************************"
